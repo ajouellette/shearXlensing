3eaec7e997147ca8c1d48a8bfe076cd50bbac71b
Aaron Ouellette <aaronjo2@illinois.edu>
2024-11-21 16:30:16 -0600

    complete rework

--- scripts/gen_shear_maps.py
+++ scripts/gen_shear_maps.py
@@ -1,77 +1,97 @@
+import argparse
 import glob
 import os
+from os import path
 import sys
 
 import numpy as np
 import healpy as hp
+from astropy.table import Table
 
-sys.path.insert(0, "/projects/caps/aaronjo2/shearXlensing/code")
-from catalogs import DESY3ShearCat
-
-
-def make_map(nside, ra, dec, vals=None):
-    ipix = hp.ang2pix(nside, ra, dec, lonlat=True)
-    npix = hp.nside2npix(nside)
-    return np.bincount(ipix, weights=vals, minlength=npix)
+sys.path.insert(0, path.join(path.dirname(path.realpath(__file__)), "../code"))
+from timer import Timer
+from misc import make_healpix_map
 
 
 def main():
 
-    nside = 2048
-    overwrite = False
+    parser = argparse.ArgumentParser()
+    parser.add_argument("cat_dir")
+    parser.add_argument("--nside", default=2048, type=int)
+    parser.add_argument("--overwrite", action="store_true")
+    parser.add_argument("-o", "--output")
+    parser.add_argument("--pattern", default="*.fits")
+    parser.add_argument("--do-psf", action="store_true")
+    args = parser.parse_args()
 
-    if len(sys.argv) < 2:
-        print("Must specify location of DES Y3 calibrated catalogs")
-        return
+    nside = args.nside
+    overwrite = args.overwrite
+    cat_dir = args.cat_dir
 
-    cat_dir = sys.argv[1]
-    cat_files = glob.glob(f"{cat_dir}/*.pkl")
+    cat_files = sorted(glob.glob(path.join(cat_dir, args.pattern)))
+    print(f"found {len(cat_files)} catalogs")
 
-    # save maps at {data_dir}/../maps
-    save_dir = '/'.join(cat_dir.rstrip('/').split('/')[:-1]) + "/maps"
+    if args.output is None:
+        # save maps at {data_dir}/../maps
+        save_dir = path.join(cat_dir, "../maps")
+    else:
+        save_dir = args.output
     print("saving maps to:", save_dir, '\n')
     os.makedirs(save_dir, exist_ok=True)
 
+    shot_noise_vals = []
     for cat_file in cat_files:
-        cat = DESY3ShearCat.load_from_pkl(cat_file)
-        print(cat.name)
+        print("Catalog:", cat_file)
+        name_parts = path.basename(path.splitext(cat_file)[0]).split('_')
+        # remove part of file name that refers to "catalog"
+        name_parts = [part for part in name_parts if "cat" not in part]
+
+        save_name_base = path.join(save_dir, '_'.join(name_parts) + f"_nside{nside}")
+        save_name_mask = save_name_base + "_mask.fits"
+        save_name_maps = save_name_base + "_shearmaps.fits"
+        save_name_psf = save_name_base + "_psfmaps.fits"
+
+        print("loading catalog...")
+        cat = Table.read(cat_file)
+        ipix = hp.ang2pix(nside, cat["ra"], cat["dec"], lonlat=True)
 
         # weights map
-        w_map = make_map(nside, cat.data["ra"], cat.data["dec"], vals=cat.data["weight"])
-        mask = w_map > 0
+        print("computing mask...")
+        w_map = make_healpix_map(nside, ipix=ipix, vals=cat["weight"])
+        hp.write_map(save_name_mask, w_map, overwrite=True)
+        mask_b = w_map > 0
 
         # weighted shear maps
-        g_maps = [make_map(nside, cat.data["ra"], cat.data["dec"], vals=cat.data[f"g_{i}"] * cat.data["weight"])
-                  for i in [1, 2]]
-        for g_map in g_maps:
-            g_map[mask] = g_map[mask] / w_map[mask]
+        if overwrite or not path.exists(save_name_maps): 
+            print("computing shear maps...")
+            g_maps = [make_healpix_map(nside, ipix=ipix, vals=cat[f"g_{i}"] * cat["weight"]) for i in [1, 2]]
+            for g_map in g_maps:
+                g_map[mask_b] = g_map[mask_b] / w_map[mask_b]
+            
+            hp.write_map(save_name_maps, g_maps, column_names=["g1", "g2"], overwrite=True)
 
         # calculate shot noise
-        sigma2_e_cat = (cat.data["g_1"]**2 + cat.data["g_2"]**2) / 2
-        w2_sigma2_map = make_map(nside, cat.data["ra"], cat.data["dec"], vals=cat.data["weight"]**2 * sigma2_e_cat)
+        print("computing shot noise...")
+        # Following Nicola et al 2020 (eqs 2.2 and 2.24)
+        sigma2_e_cat = (cat["g_1"]**2 + cat["g_2"]**2) / 2
+        w2_sigma2_map = make_healpix_map(nside, ipix=ipix, vals=cat["weight"]**2 * sigma2_e_cat)
         shot_noise = hp.nside2pixarea(nside) * np.mean(w2_sigma2_map)
+        shot_noise_vals.append(shot_noise)
         print(f"Shot noise in auto pseudo-Cl: {shot_noise:.5e}")
 
-        # psf elipticity maps
-        psf_maps = [make_map(nside, cat.data["ra"], cat.data["dec"],
-                    vals=cat.data[f"psf_e{i}"] * cat.data["weight"]) for i in [1, 2]]
-        for psf_map in psf_maps:
-            psf_map[mask] = psf_map[mask] / w_map[mask]
+        if args.do_psf:
+            if overwrite or not path.exists(save_name_psf):
+                # psf elipticity maps
+                psf_maps = [make_healpix_map(nside, ipix=ipix, vals=cat[f"psf_e{i}"] * cat["weight"]) for i in [1, 2]]
+                for psf_map in psf_maps:
+                    psf_map[mask_b] = psf_map[mask_b] / w_map[mask_b]
 
-        save_name = f"{save_dir}/DESY3_zbin{cat.zbin}_nside{nside}"
+                hp.write_map(save_name_psf, psf_maps, column_names=["psf_e1", "psf_e2"], overwrite=True)
 
-        try:
-            hp.write_map(save_name + "_shearmaps.fits", g_maps, column_names=["g1", "g2"], overwrite=overwrite)
-        except OSError:
-            pass
-        try:
-            hp.write_map(save_name + "_psfmaps.fits", psf_maps, column_names=["psf_e1", "psf_e2"], overwrite=overwrite)
-        except OSError:
-            pass
-        try:
-            hp.write_map(save_name + "_mask.fits", w_map, overwrite=overwrite)
-        except OSError:
-            pass
+    # write shot noise estimates
+    with open(path.join(save_dir, f"shot_noise_ests_nside{nside}.txt"), 'w') as f:
+        lines = [f"{cat_file}: {shot_noise_vals[i]:.5e}" for i, cat_file in enumerate(cat_files)]
+        f.write('\n'.join(lines))
 
 
 if __name__ == "__main__":
