9e23ba8cc52eb7af707a2ced5eaf7e42f3e4eca6
Aaron Ouellette <aaronjo2@illinois.edu>
2024-11-22 14:24:03 -0600

    more general code to compute arbitrary sim Cls

--- scripts/compute_sim_cls.py
+++ scripts/compute_sim_cls.py
@@ -1,7 +1,7 @@
 import argparse
 import os
 from os import path
-import time
+import sys
 
 import numpy as np
 import healpy as hp
@@ -9,19 +9,81 @@ import pymaster as nmt
 from astropy.table import Table
 import nx2pt
 
+sys.path.insert(0, path.join(path.dirname(path.realpath(__file__)), "../code"))
+from timer import Timer
+from agora_rotations import get_rotator
+
+
+# combinations needed:
+# - mock lensing x mock shear
+# - mock lensing x true lensing
+# - mock shear x mock shear
+
+
+def get_g_mock_cat(sim_dir, nside, rot):
+    pattern = "shear/mock_cats/desy3_mockcat_zbin{zbin}_rot{rot:02}.fits"
+    lmax = 3*nside - 1
+    tracers = []
+    for zbin in range(1, 5):
+        cat = Table.read(path.join(sim_dir, pattern.format(zbin=zbin, rot=rot)))
+        tracer = nx2pt.CatalogTracer(f"shear zbin {zbin} rot {rot}", [cat["ra"], cat["dec"]],
+                                     cat["weight"], lmax, fields=[cat["g_1"], cat["g_2"]])
+        tracers.append(tracer)
+    return tracers
+
+
+def get_g_mock_map(sim_dir, nside, rot, zbin):
+    pattern = "shear/mock_cats/desy3_mockcat_zbin{zbin}_rot{rot:02}.fits"
+    return tracer
+
+
+def get_g_true_map(sim_dir, nside, rot, zbin):
+    pattern = "shear/mock_cats/desy3_mockcat_zbin{zbin}_rot{rot:02}.fits"
+    return tracer
+
+
+def get_k_mock_map(sim_dir, nside, rot):
+    if nside != 2048:
+        raise NotImplementedError
+    pattern = "cmbkappa/mockobs/kmapxx_k{dataset}_50{rot:02}_1.fits"
+    cmbk_mask = "cmbkappa/mockobs/mask2048_binary_inner_smoothmask_apod_v2.fits"
+    cmbk_mask = hp.read_map(path.join(sim_dir, cmbk_mask))
+    tracers = []
+    for dataset in ["GMV", "PP"]:
+        cmbk = hp.read_map(path.join(args.sim_dir, pattern.format(dataset=dataset, rot=rot)))
+        tracer = nx2pt.MapTracer(f"CMB kappa ({dataset})", [cmbk], cmbk_mask**2, masked_on_input=True)
+        tracers.append(tracer)
+    return tracers
+
+
+def get_k_true_map(sim_dir, nside, rot):
+    if nside != 2048:
+        raise NotImplementedError
+
+
+fields = {
+        "shear_mock_cat": get_g_mock_cat,
+        "shear_mock_map": get_g_mock_map,
+        "shear_true": get_g_true_map,
+        "cmbk_mock": get_k_mock_map,
+        "cmbk_true": get_k_true_map,
+    }
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("sim_dir")
+    parser.add_argument("field1")
+    parser.add_argument("field2")
     parser.add_argument("--nside", default=2048, type=int)
     parser.add_argument("--overwrite", action="store_true")
-    #parser.add_argument("
     args = parser.parse_args()
 
-    cmbk_pattern = "cmbkappa/mockobs/kmapxx_k{dataset}_50{rot:02}_1.fits"
-    cmbk_mask = "cmbkappa/mockobs/mask2048_binary_inner_smoothmask_apod_v2.fits"
-    shear_pattern = "shear/mock_cats/desy3_mockcat_zbin{zbin}_rot{rot:02}.fits"
+    if args.field1 not in fields:
+        raise ValueError(f"unknown field {args.field1}")
+    if args.field2 not in fields:
+        raise ValueError(f"unknown field {args.field2}")
+
     wksp_cache = "/home/aaronjo2/scratch/nmt_workspaces"
     output_dir = path.join(args.sim_dir, "cross_corrs")
     os.makedirs(output_dir, exist_ok=True)
@@ -30,54 +92,49 @@ if __name__ == "__main__":
     bins = nx2pt.get_nmtbins(args.nside, bpw_edges)
     ell_eff = bins.get_effective_ells()
 
-    datasets = ["GMV", "PP"]
-    zbins = list(range(1, 5))
     rotations = list(range(1, 11))
+    cls_all = []
+    bpws_all = []
+    for rot_i, rot in enumerate(rotations):
+        print(f"---\nRotation {rot}\n---")
 
-    cmbk_mask = hp.ud_grade(hp.read_map(path.join(args.sim_dir, cmbk_mask)), args.nside)
-    
-    for dataset in datasets:
-        print(f"Dataset: {dataset}")
-        for zbin in zbins:
-            print(f"z-bin: {zbin}")
-            output_file = path.join(output_dir, f"cmbk_{dataset}_shear_zbin{zbin}_nside{args.nside}_masked_on_input_cls.npz")
-            if path.exists(output_file) and not args.overwrite:
-                print(f"{output_file} already exists, skipping")
-                continue
-            
+        output_file = path.join(output_dir, f"{args.field1}_{args.field2}_nside{args.nside}_cls.npz")
+        if path.exists(output_file) and not args.overwrite:
+            print(f"{output_file} already exists, skipping")
+            continue
+
+        with Timer("loading fields..."):
+            fields1 = fields[args.field1](args.sim_dir, args.nside, rot)
+            print("Field 1:", fields1)
+            if args.field2 == args.field1:
+                fields2 = fields1
+            else:
+                fields2 = fields[args.field2](args.sim_dir, args.nside, rot)
+            print("Field 2:", fields2)
+        
+        # calculate all cross-spectra
+        cls_rot = []
+        bpws_rot = []
+        for i, field1 in enumerate(fields1):
             cls = []
-            for rot in rotations:
-                print(f"---\nRotation {rot}\n---")
-                print("reading data...")
-                t1 = time.perf_counter()
-                cmbk = hp.read_map(path.join(args.sim_dir, cmbk_pattern.format(dataset=dataset, rot=rot)))
-                shear = Table.read(path.join(args.sim_dir, shear_pattern.format(zbin=zbin, rot=rot)))
-                t2 = time.perf_counter()
-                print(f"  ({t2 - t1:.1f} s)")
-                print("constructing tracers...")
-                t1 = time.perf_counter()
-                cmbk = nx2pt.MapTracer(f"CMB kappa ({dataset})", [cmbk], cmbk_mask**2, masked_on_input=True)
-                print(cmbk)
-                shear = nx2pt.CatalogTracer(f"DES shear (bin {zbin})", [shear["ra"], shear["dec"]], shear["weight"],
-                                            3*args.nside-1, fields=[shear["g_1"], shear["g_2"]])
-                print(shear)
-                t2 = time.perf_counter()
-                print(f"  ({t2 - t1:.1f} s)")
-                print("getting workspace...")
-                t1 = time.perf_counter()
-                wksp = nx2pt.get_workspace(cmbk.field, shear.field, bins, wksp_cache=wksp_cache)
-                t2 = time.perf_counter()
-                print(f"  ({t2 - t1:.1f} s)")
-                print("computing Cl...")
-                t1 = time.perf_counter()
-                cl = nmt.compute_coupled_cell(cmbk.field, shear.field)
-                cl = wksp.decouple_cell(cl)
-                t2 = time.perf_counter()
-                print(f"  ({t2 - t1:.1f} s)")
+            bpws = []
+            for j, field2 in enumerate(fields2):
+                # skip non-unique cross-spectra
+                if field1 == field2 and j < i:
+                    continue
+                with Timer(f"computing Cl {i} x {j}..."):
+                    wksp = nx2pt.get_workspace(field1.field, field2.field, bins, wksp_cache=wksp_cache)
+                    cl = nmt.compute_coupled_cell(field1.field, field2.field)
+                    cl = wksp.decouple_cell(cl)
                 cls.append(cl)
-    
-            cls = np.array(cls)
-            bpws = wksp.get_bandpower_windows()
-
-            print("saving to", output_file)
-            np.savez(output_file, cls=cls, ell_eff=ell_eff, bpws=bpws)
+                bpws.append(wksp.get_bandpower_windows())
+            cls_rot.append(cls)
+            bpws_rot.append(bpws)
+        cls_all.append(cls_rot)
+        bpws_all.append(bpws_rot)
+            
+    # save cross-spectra and bandpowers
+    cls_all = np.array(cls_all)
+    bpws_all = np.array(bpws_all)
+    print("saving to", output_file)
+    np.savez(output_file, cls=cls_all, ell_eff=ell_eff, bpws=bpws_all)
