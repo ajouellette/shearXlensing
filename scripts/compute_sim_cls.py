51221b516b9fc9395f82ac3bde08083594f7a08f
Aaron Ouellette <aaronjo2@illinois.edu>
2024-12-13 10:49:24 -0600

    generalize sim cls script

--- scripts/compute_sim_cls.py
+++ scripts/compute_sim_cls.py
@@ -32,14 +32,34 @@ def get_g_mock_cat(sim_dir, nside, rot):
     return tracers
 
 
-def get_g_mock_map(sim_dir, nside, rot, zbin):
-    pattern = "shear/mock_cats/desy3_mockcat_zbin{zbin}_rot{rot:02}.fits"
-    return tracer
+def get_g_mock_map(sim_dir, nside, rot):
+    pattern = "shear/mock_maps/desy3_zbin{zbin}_rot{rot:02}_nside{nside}_{map}.fits"
+    shot_noise_file = path.join(sim_dir, f"shear/mock_maps/shot_noise_ests_nside{nside}.txt")
+    shot_noise_ests = {}
+    if path.exists(shot_noise_file):
+        with open(shot_noise_file) as f:
+            for line in f.readlines():
+                key, val = line.split(": ")
+                shot_noise_ests[key] = float(val)
+    tracers = []
+    for zbin in range(1, 5):
+        mask = hp.read_map(path.join(sim_dir, pattern.format(zbin=zbin, rot=rot, nside=nside, map="mask")))
+        maps = hp.read_map(path.join(sim_dir, pattern.format(zbin=zbin, rot=rot, nside=nside, map="shearmaps")), field=None)
+        tracer = nx2pt.MapTracer(f"shear zbin {zbin} rot {rot}", maps, mask, beam=hp.pixwin(nside))
+        for key in shot_noise_ests:
+            if f"zbin{zbin}_rot{rot:02}" in key:
+                tracer.shot_noise = shot_noise_ests[key]
+                break
+        if not hasattr(tracer, "shot_noise"):
+            print("Warning: no shot noise estimate")
+        tracers.append(tracer)
+    return tracers
 
 
 def get_g_true_map(sim_dir, nside, rot, zbin):
     pattern = "shear/mock_cats/desy3_mockcat_zbin{zbin}_rot{rot:02}.fits"
-    return tracer
+    tracers = []
+    return tracers
 
 
 def get_k_mock_map(sim_dir, nside, rot):
@@ -77,6 +97,10 @@ if __name__ == "__main__":
     parser.add_argument("field2")
     parser.add_argument("--nside", default=2048, type=int)
     parser.add_argument("--overwrite", action="store_true")
+    parser.add_argument("--lmin", type=int, default=30)
+    parser.add_argument("--lmax", type=int, default=3800)
+    parser.add_argument("--n-bins", type=int, default=24)
+    parser.add_argument("--binning", default="linear", choices=["linear", "sqrt", "log"])
     args = parser.parse_args()
 
     if args.field1 not in fields:
@@ -88,20 +112,20 @@ if __name__ == "__main__":
     output_dir = path.join(args.sim_dir, "cross_corrs")
     os.makedirs(output_dir, exist_ok=True)
 
-    bpw_edges = nx2pt.get_bpw_edges(30, 3800, 24, "linear")
+    bpw_edges = nx2pt.get_bpw_edges(args.lmin, args.lmax, args.n_bins, args.binning)
     bins = nx2pt.get_nmtbins(args.nside, bpw_edges)
     ell_eff = bins.get_effective_ells()
+
+    output_file = path.join(output_dir, f"{args.field1}_{args.field2}_nside{args.nside}_cls.npz")
+    if path.exists(output_file) and not args.overwrite:
+        print(f"{output_file} already exists, skipping")
+        sys.exit()
 
     rotations = list(range(1, 11))
     cls_all = []
     bpws_all = []
     for rot_i, rot in enumerate(rotations):
         print(f"---\nRotation {rot}\n---")
-
-        output_file = path.join(output_dir, f"{args.field1}_{args.field2}_nside{args.nside}_cls.npz")
-        if path.exists(output_file) and not args.overwrite:
-            print(f"{output_file} already exists, skipping")
-            continue
 
         with Timer("loading fields..."):
             fields1 = fields[args.field1](args.sim_dir, args.nside, rot)
@@ -119,12 +143,18 @@ if __name__ == "__main__":
             cls = []
             bpws = []
             for j, field2 in enumerate(fields2):
-                # skip non-unique cross-spectra
-                if field1 == field2 and j < i:
-                    continue
                 with Timer(f"computing Cl {i} x {j}..."):
                     wksp = nx2pt.get_workspace(field1.field, field2.field, bins, wksp_cache=wksp_cache)
                     cl = nmt.compute_coupled_cell(field1.field, field2.field)
+                    if field1 == field2 and hasattr(field1, "shot_noise"):
+                        print("subtracting shot noise...")
+                        if field1.spin == 0:
+                            cl -= field1.shot_noise
+                        else:
+                            shot_noise = np.zeros_like(cl)
+                            shot_noise[0] = field1.shot_noise * np.ones(len(cl[0]))
+                            shot_noise[-1] = field1.shot_noise * np.ones(len(cl[0]))
+                            cl -= shot_noise
                     cl = wksp.decouple_cell(cl)
                 cls.append(cl)
                 bpws.append(wksp.get_bandpower_windows())
