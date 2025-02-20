5f4569eca460f6299630bfa2a824814db38d5e19
Aaron Ouellette <aaronjo2@illinois.edu>
2025-02-20 13:46:12 -0600

    major updates

--- scripts/compute_sim_cls.py
+++ scripts/compute_sim_cls.py
@@ -1,4 +1,5 @@
 import argparse
+from functools import partial
 import os
 from os import path
 import sys
@@ -12,12 +13,6 @@ import nx2pt
 sys.path.insert(0, path.join(path.dirname(path.realpath(__file__)), "../code"))
 from timer import Timer
 from agora_rotations import get_rotator
-
-
-# combinations needed:
-# - mock lensing x mock shear
-# - mock lensing x true lensing
-# - mock shear x mock shear
 
 
 def get_g_mock_cat(sim_dir, nside, rot):
@@ -62,43 +57,84 @@ def get_g_true_map(sim_dir, nside, rot, zbin):
     return tracers
 
 
-def get_k_mock_map(sim_dir, nside, rot):
+def get_k_mock_map(sim_dir, nside, rot, binary_mask=False, trunc_mask=False, new=False):
     if nside != 2048:
         raise NotImplementedError
     pattern = "cmbkappa/mockobs/kmapxx_k{dataset}_50{rot:02}_1.fits"
     cmbk_mask = "cmbkappa/mockobs/mask2048_binary_inner_smoothmask_apod_v2.fits"
+    cmbk_mask_trunc = "cmbkappa/mockobs/mask2048_binary_inner_smoothmask_apod_v2_trunc2.5.fits"
     cmbk_mask = hp.read_map(path.join(sim_dir, cmbk_mask))
+    if trunc_mask:
+        apply_mask = hp.read_map(path.join(sim_dir, cmbk_mask_trunc))
+    else:
+        apply_mask = cmbk_mask
+    if binary_mask:
+        apply_mask = apply_mask == 1
     tracers = []
     for dataset in ["GMV", "PP"]:
-        cmbk = hp.read_map(path.join(args.sim_dir, pattern.format(dataset=dataset, rot=rot)))
-        tracer = nx2pt.MapTracer(f"CMB kappa ({dataset})", [cmbk], cmbk_mask**2, masked_on_input=True)
+        if new:
+            if dataset == "PP":
+                continue
+            cmbk = hp.read_map(path.join(sim_dir, pattern.replace("mockobs", "agorarec").format(dataset=dataset, rot=rot)))
+        else:
+            cmbk = hp.read_map(path.join(sim_dir, pattern.format(dataset=dataset, rot=rot)))
+        tracer = nx2pt.MapTracer(f"CMB kappa ({dataset})", [cmbk * apply_mask], cmbk_mask**2 * apply_mask,
+                                masked_on_input=True)
         tracers.append(tracer)
     return tracers
 
 
-def get_k_true_map(sim_dir, nside, rot):
+def get_k_true_map(sim_dir, nside, rot, masked=False, binary_mask=False, new=False):
+    cmbk_mask = "cmbkappa/mockobs/mask2048_binary_inner_smoothmask_apod_v2.fits"
     if nside != 2048:
         raise NotImplementedError
+    klm = "cmbkappa/sim_alms/agora_raytrace16384_cmbkappa_highzadded_lowLcorrected_cmbkappa_alm.fits"
+    if new:
+        with np.load(path.join(sim_dir, "cmbkappa/agorarec/klm_rotated_v2.npz")) as f:
+            klm = f[str(rot)]
+    else:
+        with np.load(path.join(sim_dir, "cmbkappa/sim_alms/rotated_alms.npz")) as f:
+            klm = f[str(rot)]
+    cmbk_true = hp.alm2map(klm, nside)
+    if masked:
+        cmbk_mask = hp.read_map(path.join(sim_dir, cmbk_mask))
+        if binary_mask:
+            cmbk_mask = cmbk_mask == 1
+    else:
+        cmbk_mask = np.ones_like(cmbk_true)
+    tracer = nx2pt.MapTracer("simulated CMB kappa", [cmbk_true], cmbk_mask)
+    return [tracer]
 
 
 fields = {
         "shear_mock_cat": get_g_mock_cat,
         "shear_mock_map": get_g_mock_map,
         "shear_true": get_g_true_map,
+
         "cmbk_mock": get_k_mock_map,
+        "cmbk_mock_bmask": partial(get_k_mock_map, binary_mask=True),
+        "cmbk_mock_trunc": partial(get_k_mock_map, trunc_mask=True),
         "cmbk_true": get_k_true_map,
+        "cmbk_true_masked": partial(get_k_true_map, masked=True),
+        "cmbk_true_bmasked": partial(get_k_true_map, masked=True, binary_mask=True),
+
+        "cmbk_mock_new": partial(get_k_mock_map, new=True),
+        "cmbk_mock_new_trunc": partial(get_k_mock_map, new=True, trunc_mask=True),
+        "cmbk_true_new": partial(get_k_true_map, new=True),
+        "cmbk_true_new_masked": partial(get_k_true_map, new=True, masked=True),
     }
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+
+    parser = argparse.ArgumentParser(epilog=f"Possible fields: {list(fields.keys())}")
     parser.add_argument("sim_dir")
     parser.add_argument("field1")
     parser.add_argument("field2")
     parser.add_argument("--nside", default=2048, type=int)
     parser.add_argument("--overwrite", action="store_true")
     parser.add_argument("--lmin", type=int, default=30)
-    parser.add_argument("--lmax", type=int, default=3800)
+    parser.add_argument("--lmax", type=int, default=3500)
     parser.add_argument("--n-bins", type=int, default=24)
     parser.add_argument("--binning", default="linear", choices=["linear", "sqrt", "log"])
     args = parser.parse_args()
@@ -108,7 +144,7 @@ if __name__ == "__main__":
     if args.field2 not in fields:
         raise ValueError(f"unknown field {args.field2}")
 
-    wksp_cache = "/home/aaronjo2/scratch/nmt_workspaces"
+    wksp_cache = "/scratch/aaronjo2/nmt_workspaces"
     output_dir = path.join(args.sim_dir, "cross_corrs")
     os.makedirs(output_dir, exist_ok=True)
 
@@ -139,14 +175,30 @@ if __name__ == "__main__":
         # calculate all cross-spectra
         cls_rot = []
         bpws_rot = []
+        covs_rot = []
         for i, field1 in enumerate(fields1):
             cls = []
             bpws = []
+            covs = []
             for j, field2 in enumerate(fields2):
-                with Timer(f"computing Cl {i} x {j}..."):
+                with Timer(f"computing Cl {field1.name} x {field2.name}..."):
                     wksp = nx2pt.get_workspace(field1.field, field2.field, bins, wksp_cache=wksp_cache)
                     cl = nmt.compute_coupled_cell(field1.field, field2.field)
-                    if field1 == field2 and hasattr(field1, "shot_noise"):
+
+                    #if rot_i == 0:
+                    #    with Timer("computing cov..."):
+                    #        # compute covariance for single realization (spin-0 approx)
+                    #        cov_wksp = nx2pt.get_cov_workspace(field1.field, field2.field, wksp_cache=wksp_cache)
+                    #        pcl1 = nmt.compute_coupled_cell(field1.field, field1.field)
+                    #        fsky1 = np.mean(field1.mask**2)
+                    #        pcl2 = nmt.compute_coupled_cell(field2.field, field2.field)
+                    #        fsk2 = np.mean(field2.mask**2)
+                    #        fsky_cross = np.mean(field1.mask * field2.mask)
+
+                    #        cov = nmt.gaussian_covariance(cov_wksp, *[0,]*4, pcl1[:1] / fsky1, *[cl[:1] / fsky_cross,]*2, pcl2[:1] / fsky2, wksp)
+                    #        covs.append(cov)
+
+                    if field1 is field2 and hasattr(field1, "shot_noise"):
                         print("subtracting shot noise...")
                         if field1.spin == 0:
                             cl -= field1.shot_noise
@@ -160,11 +212,13 @@ if __name__ == "__main__":
                 bpws.append(wksp.get_bandpower_windows())
             cls_rot.append(cls)
             bpws_rot.append(bpws)
+            covs_rot.append(covs)
         cls_all.append(cls_rot)
         bpws_all.append(bpws_rot)
             
     # save cross-spectra and bandpowers
     cls_all = np.array(cls_all)
     bpws_all = np.array(bpws_all)
+    covs_all = np.array(covs_rot)
     print("saving to", output_file)
     np.savez(output_file, cls=cls_all, ell_eff=ell_eff, bpws=bpws_all)
