34f0a665ef7afcbba6a18f760ad861a8ddb2cd86
Aaron Ouellette <aaronjo2@illinois.edu>
2025-03-30 22:40:35 -0500

    revamp catalog calibration script

--- scripts/gen_calibrated_catalogs.py
+++ scripts/gen_calibrated_catalogs.py
@@ -1,22 +1,28 @@
+import argparse
 import os
 from os import path
 import sys
 import joblib
 
-sys.path.insert(0, "/projects/caps/aaronjo2/shearXlensing/code")
+sys.path.insert(0, "/projects/ncsa/caps/aaronjo2/shearXlensing/code")
 from catalogs import DESY3ShearCat
 
 
 def main():
-    if len(sys.argv) < 2:
-        print("Must specify location of DES Y3 catalogs index file")
-        return
+    parser = argparse.ArgumentParser()
+    parser.add_argument("index_file")
+    parser.add_argument("--sample", default="all", choices=["all", "blue", "red"])
+    parser.add_argument("-o", "--output")
+    parser.add_argument("--overwrite", action="store_true")
+    args = parser.parse_args()
 
-    index_file = sys.argv[1]
+    index_file = args.index_file
 
-
-    # save catalogs at {data_dir}/../calibrated_catalogs
-    save_dir = path.join(path.dirname(index_file), '..', "calibrated_catalogs")
+    if args.output is None:
+        # save catalogs at {data_dir}/../calibrated_catalogs
+        save_dir = path.join(path.dirname(index_file), '..', "calibrated_catalogs")
+    else:
+        save_dir = args.output
     print("Will save calibrated catalogs to:", save_dir, '\n')
     os.makedirs(save_dir, exist_ok=True)
 
@@ -24,17 +30,23 @@ def main():
         if zbin is not None:
             print("z-bin", zbin)
         else:
+            if args.sample != "all":
+                continue
             print("Combined bin")
-
-        print("loading catalog and calibrating...")
-        cat = DESY3ShearCat(index_file, zbin=zbin)
-        print(len(cat.data), "galaxies")
-        print("total multiplicative bias:", cat.R)
 
         if zbin is not None:
             save_file = path.join(save_dir, f"DESY3_shearcat_zbin{zbin-1}.fits")
         else:
             save_file = path.join(save_dir, f"DESY3_shearcat_combined.fits")
+        if path.isfile(save_file) and not args.overwrite:
+            print(f"{save_file} already exists, skipping")
+            continue
+
+        print("loading catalog and calibrating...")
+        cat = DESY3ShearCat(index_file, zbin=zbin, sample=args.sample)
+        print(len(cat.data), "galaxies")
+        print("total multiplicative bias:", cat.R)
+
         print("writing catalog to", save_file)
         # save catalog as astropy table in FITS format
         cat.data.write(save_file, overwrite=True)
