ccb38b7cb856bb6d0a55d54d903d5870c93c7c4f
Aaron Ouellette <aaronjo2@illinois.edu>
2025-05-12 11:28:13 -0500

    specify shear noise level + code updates

--- scripts/make_mock_cats.py
+++ scripts/make_mock_cats.py
@@ -2,22 +2,14 @@ import argparse
 import glob
 import os
 from os import path
+import sys
 import numpy as np
 import healpy as hp
 from astropy.table import Table
 
-
-# list of rotation angles to match mock lensing reconstructions
-rotations = [[0, 0, 0],
-             [0, 180, 0],
-             [180, 180, 0],
-             [0, 180, 180],
-             [0, 60, 90],
-             [0, 120, 90],
-             [0, 180, 90],
-             [0, 240, 90],
-             [0, 300, 90],
-             [0, 360, 90]]
+sys.path.insert(0, "/projects/ncsa/caps/aaronjo2/shearXlensing/code")
+from agora_rotations import get_rotator
+from timer import Timer
 
 
 def rotate_e1e2_rand(e1, e2, seed=None):
@@ -27,15 +19,15 @@ def rotate_e1e2_rand(e1, e2, seed=None):
     return rot.real, rot.imag
 
 
-def gen_shear_catalog(signal_maps, catalog, seed=None):
+def gen_shear_catalog(signal_maps, catalog, seed=None, noise_level=1):
     nside = hp.npix2nside(len(signal_maps[0]))
     cat_inds = hp.ang2pix(nside, catalog["ra"], catalog["dec"], lonlat=True)
     # generate noise
     g1_n, g2_n = rotate_e1e2_rand(catalog["g_1"], catalog["g_2"], seed=seed)
     # signal + noise
     # here we just sample the value of the pixel, should be ok if nside is high enough
-    g1 = signal_maps[0][cat_inds] + g1_n
-    g2 = signal_maps[1][cat_inds] + g2_n
+    g1 = signal_maps[0][cat_inds] + noise_level * g1_n
+    g2 = signal_maps[1][cat_inds] + noise_level * g2_n
     # new catalog
     sim_cat = Table(dict(ra=catalog["ra"], dec=catalog["dec"],
                          weight=catalog["weight"], g_1=g1, g_2=g2))
@@ -46,11 +38,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("sim_dir")
     parser.add_argument("cat_dir")
+    parser.add_argument("-o", "--output")
     parser.add_argument("--no-ia", action="store_true")
+    parser.add_argument("--noise_level", default=1, type=float, help="noise level relative to that of provided catalogs")
     args = parser.parse_args()
 
-    shear_pattern = "shear/raytrace_kg1g2_*_fullsky.fits"
-    ia_pattern = "shear/raytrace_IAkg1g2_*_fullsky*.fits"
+    shear_pattern = "shear/fullsky/maps/raytrace_kg1g2_*_fullsky.fits"
+    ia_pattern = "shear/fullsky/maps/raytrace_IAkg1g2_*_fullsky*.fits"
     cat_pattern = "DESY3_shearcat_zbin*.fits"
 
     # find files
@@ -85,21 +79,21 @@ if __name__ == "__main__":
 
         catalog = Table.read(cat_files[zbin])
 
-        for i, rot_angles in enumerate(rotations):
-            print(f"Rotation {i+1}: {rot_angles}")
+        for i in range(10):
+            print(f"Rotation {i+1}:")
             
-            save_name = path.join(args.sim_dir, f"shear/mock_cats/desy3_mockcat_zbin{zbin+1}_rot{i+1:02}.fits")
+            save_name = path.join(args.output, f"desy3_mockcat_zbin{zbin+1}_rot{i+1:02}.fits")
             if os.path.exists(save_name):
                 print(f"{save_name} exists, skipping")
                 continue
 
-            rot = hp.Rotator(rot=rot_angles, deg=True, inv=True)
+            rot = get_rotator(i)
             # rotate signal maps
-            print("rotating signal map")
-            rot_signal = rot.rotate_map_pixel(shear_maps)
+            with Timer("rotating signal map"):
+                rot_signal = rot.rotate_map_pixel(shear_maps)
 
-            print("generating catalog")
-            sim_cat = gen_shear_catalog(rot_signal, catalog)
+            with Timer("generating catalog"):
+                sim_cat = gen_shear_catalog(rot_signal, catalog, noise_level=args.noise_level)
 
             # save catalog
             os.makedirs(path.dirname(save_name), exist_ok=True)
